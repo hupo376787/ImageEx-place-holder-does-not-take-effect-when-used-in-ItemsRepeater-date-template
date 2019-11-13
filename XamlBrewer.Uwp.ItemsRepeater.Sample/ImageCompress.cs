c82ffed461f2e215edc0a1b373bd2699bac76745
Vincent Wang <376787823@qq.com>
2019-11-13 14:42:34 +0800

    Add files via upload

---
+++ XamlBrewer.Uwp.ItemsRepeater.Sample/ImageCompress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace XamlBrewer.Uwp.ItemsRepeater.Sample
+{
+    public class ImageCompress : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new BitmapImage(new Uri("https://www.viu.com/ott/hk/v1/imgprocess/reduceImage.php?p=50&img=" + value.ToString()));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
